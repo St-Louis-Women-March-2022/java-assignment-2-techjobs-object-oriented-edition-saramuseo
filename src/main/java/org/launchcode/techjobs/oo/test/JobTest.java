859b0e985d06873b7af75fd741897bc3cad1a190
Sara Davis <smartinez.davis239@gmail.com>
2022-09-11 10:42:53 -0500

    task 4 completed and tested

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,55 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+
+    @Test
+    public void testSettingJobId () {
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertNotEquals(job1.getId() == job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+    Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    assertTrue(job3 instanceof Job);
+    assertTrue(job3.getEmployer() instanceof Employer);
+    assertTrue(job3.getLocation() instanceof Location);
+    assertTrue(job3.getPositionType() instanceof PositionType);
+    assertTrue(job3.getCoreCompetency() instanceof CoreCompetency);
+    assertEquals("Product tester", job3.getName());
+    assertEquals("ACME", job3.getEmployer().getValue());
+    assertEquals("Desert", job3.getLocation().getValue());
+    assertEquals("Quality control", job3.getPositionType().getValue());
+    assertEquals("Persistence", job3.getCoreCompetency().getValue());
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        Job job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job job5 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertFalse(job4.equals(job5));
+    }
+
+
+    private void assertNotEquals(boolean b) {
+    }
+
+
 
 }
